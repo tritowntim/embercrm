889fcd01757459882758666f5c0aca2a6ff9a286
Tim Dussinger <tritowntim@gmail.com>
2014-08-21 22:39:10 -0400

    Setup Ember app, version, directories

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Ember.js version
+  config.ember.variant = :development
+
 end
