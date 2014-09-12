512440e8072883c430c54cb8d408eaae4cce81e0
Tim Dussinger <tritowntim@gmail.com>
2014-09-11 22:08:40 -0400

    Missing commas

--- app/assets/javascripts/models/lead.js
+++ app/assets/javascripts/models/lead.js
@@ -1,8 +1,8 @@
 App.Lead = DS.Model.extend({
-  firstName: DS.attr('string')
-  lastName:  DS.attr('string')
-  email:     DS.attr('string')
-  phone:     DS.attr('string')
-  status:    DS.attr('string', { defaultValue: 'new' })
+  firstName: DS.attr('string'),
+  lastName:  DS.attr('string'),
+  email:     DS.attr('string'),
+  phone:     DS.attr('string'),
+  status:    DS.attr('string', { defaultValue: 'new' }),
   notes:     DS.attr('string')
 });
