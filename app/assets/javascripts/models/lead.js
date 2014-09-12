fec10002c9e02f46c7759faef18bd1e4c35e41f2
Tim Dussinger <tritowntim@gmail.com>
2014-09-12 07:33:01 -0400

    Display full name as computed property

--- app/assets/javascripts/models/lead.js
+++ app/assets/javascripts/models/lead.js
@@ -4,5 +4,8 @@ App.Lead = DS.Model.extend({
   email:     DS.attr('string'),
   phone:     DS.attr('string'),
   status:    DS.attr('string', { defaultValue: 'new' }),
-  notes:     DS.attr('string')
+  notes:     DS.attr('string'),
+  fullName:  function() {
+    return this.get('firstName') + ' ' + this.get('lastName');
+  }.property('firstName', 'lastName')
 });
