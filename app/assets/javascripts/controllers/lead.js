af68414f89e62d4df5c8b194f9af329404f2a0f6
Tim Dussinger <tritowntim@gmail.com>
2014-09-16 09:00:19 -0400

    Link to delete records

--- app/assets/javascripts/controllers/lead.js
+++ app/assets/javascripts/controllers/lead.js
@@ -9,6 +9,13 @@ App.LeadController = Ember.ObjectController.extend({
   actions: {
     saveChanges: function() {
       this.get('model').save();
+    },
+
+    delete: function() {
+      var self = this;
+      this.get('model').destroyRecord().then(function() {
+        self.transitionToRoute('leads');
+      });
     }
   }
 
